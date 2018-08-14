0ae49e07c350f31291141247a6eb536e7fc520f7
qiankaijie <qiankaijie1024@gmail.com>
2018-08-14 23:26:23 +0800

    ui fixed

--- src/uiDevelop.js
+++ src/uiDevelop.js
@@ -4,9 +4,16 @@ import './components/Posts/posts.css';
 import homePageDom from './components/HomePage/homepage.html';
 import topNavDom from './components/TopNav/nav.html';
 import postsDom from './components/Posts/posts.html';
+document.write('<div style="color:red;border:1px dashed red;">topNavDom 组件样式 start</div>');
 document.write(topNavDom);
+document.write('<div style="margin-bottom:60px;color:red;border:1px dashed red;">topNavDom组件样式 组件样式 end</div>');
+document.write('<div style="color:red;border:1px dashed red;">homePageDom 组件样式 start</div>');
 document.write(homePageDom);
+document.write('<div style="margin-bottom:60px;color:red;border:1px dashed red;">homePageDom 组件样式 end</div>');
+document.write('<div style="color:red;border:1px dashed red;">postsDom 组件样式 start</div>');
 document.write(postsDom);
+document.write('<div style="color:red;border:1px dashed red;">postsDom 组件样式 start</div>');
+
 
 import './components/TopNav/nav.js';
 import './components/HomePage/homepage.js';
